f1a47eaff828205a1fc3211480a42d56a633b5ec
alvexx45 <bernardoalves222@gmail.com>
2025-02-05 21:00:12 -0300

    conv int (fazer)

---
+++ beecrowd/1581 - Conversa Internacional/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+class Main {
+    public static String idioma(String lang) {
+        String res = "";
+        
+        return res;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            int k = sc.nextInt();
+            sc.nextLine();
+            for (int j = 0; j < k; j++) {
+                String lang = sc.nextLine();
+                idioma(lang);
+            }
+        }
+
+        sc.close();
+    }
+}
