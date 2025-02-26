467d4a28febd2e8e1a27df82aa3b7d3513f6f352
alvexx45 <bernardoalves222@gmail.com>
2025-02-26 18:58:31 -0300

    q21 need to fix

--- tps/tp1/q21/main.c
+++ tps/tp1/q21/main.c
@@ -2,18 +2,28 @@
 #include <stdlib.h>
 #include <string.h>
 
-char inverter(char txt[31]) {
-    char invertida[31];
+void inverter(char *str, int inicio, int fim) {
+    if (inicio >= fim) {
+        return;
+    }
 
-    if 
+    char tmp = str[inicio];
+    str[inicio] = str[fim];
+    str[fim] = tmp;
 
-    return invertida ;
+    inverter(str, inicio + 1, fim - 1);
 } 
 
 int main() {
     char txt[31];
-    scanf("%s")
+    scanf("%s", txt);
 
+    while(strcmp(txt, "FIM") != 0) {
+        inverter(txt, 0, strlen(txt) - 1);
+        printf("%s\n", txt);
+
+        scanf("%s", txt);
+    }
 
     return 0;
 }
