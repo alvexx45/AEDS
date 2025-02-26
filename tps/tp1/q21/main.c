aacdbccaa2f93b272bfe1aad68be5a91159e7432
alvexx45 <bernardoalves222@gmail.com>
2025-02-26 18:40:07 -0300

    create q21

---
+++ tps/tp1/q21/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char inverter(char txt[31]) {
+    char invertida[31];
+
+    if 
+
+    return invertida ;
+} 
+
+int main() {
+    char txt[31];
+    scanf("%s")
+
+
+    return 0;
+}
