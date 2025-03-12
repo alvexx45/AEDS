b7e9e0154f087f7e00695bc250d7267109588769
alvexx45 <bernardoalves222@gmail.com>
2025-03-12 17:27:41 -0300

    pesqbin iterativa

--- codigos/pesquisa/binsearch.c
+++ codigos/pesquisa/binsearch.c
@@ -1,2 +1,21 @@
-int esq = 0 dir = size - 1;
+#include <stdbool.h>
 
+bool binsearch(int array[], int n, int x) {
+    bool resp = false;
+    int esq = 0, dir = n - 1, meio;
+
+    while (esq <= dir) {
+        meio = (esq + dir) / 2;
+
+        if (array[meio] == x) {
+            resp = true;
+            esq = n;
+        } else if (array[meio] > x) {
+            esq = meio + 1;
+        } else {
+            dir = meio - 1;
+        }
+    }
+
+    return resp;
+}
