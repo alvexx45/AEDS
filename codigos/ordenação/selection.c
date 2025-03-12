671329e3b6041d45ee0f2b839eb9a4d14335a690
alvexx45 <bernardoalves222@gmail.com>
2025-03-12 19:28:02 -0300

    seleçao mod

--- codigos/ordenação/selection.c
+++ codigos/ordenação/selection.c
@@ -1,4 +1,4 @@
-int selection(int array[], int n) {
+void selection(int array[], int n) {
     int mov = 0;
 
     for (int i = 0; i < n - 1; i++) {
@@ -13,4 +13,26 @@ int selection(int array[], int n) {
         array[menor] = tmp;
         mov += 3;
     }    
+}
+
+void selectionmod(int array[], int n) {
+    int esq = 0;
+    int dir = n;
+
+    while (esq < dir) {
+        int menor = esq;
+        int maior = esq;
+
+        for (int i = esq; i < dir; i++) {
+            if (array[i] < array[menor]) menor = i;
+            if (array[i] > array[maior]) maior = i;
+        }
+        swap(array, menor, esq); // menor no inicio
+
+        if (maior == esq) maior = menor;
+        swap(array, maior, dir - 1); // maior no final
+
+        esq++; // reduz subarr na esq
+        dir--; // reduz subarr na dir
+    }
 }
