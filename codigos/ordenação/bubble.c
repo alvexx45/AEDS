c11aee3c1da5bae96f9a10219175b7e3575ff873
alvexx45 <bernardoalves222@gmail.com>
2025-03-12 19:03:47 -0300

    bbsort otimizado

--- codigos/ordenação/bubble.c
+++ codigos/ordenação/bubble.c
@@ -1,3 +1,5 @@
+#include <stdbool.h>
+
 void bubble(int array[], int n) {
     for (int i = 0; i < n-1; i++) {
         for (int j = 0; j < n-i-1; j++) {
@@ -5,5 +7,22 @@ void bubble(int array[], int n) {
                 swap(j, j + 1);
             }
         }
+    }
+}
+
+void bubbleopt(int array[], int n) {
+    bool trocado;
+
+    for (int i = 0; i < n-1; i++) {
+        trocado = false;
+
+        for (int j = 0; j < n-i-1; j++) {
+            if (array[j] > array[j + 1]) {
+                swap(j, j + 1);
+                trocado = true; // marca que houve troca
+            }
+        }
+
+        if (trocado == false) i = n-1; // array já ordenado
     }
 }
