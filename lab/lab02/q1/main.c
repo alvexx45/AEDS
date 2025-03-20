041019f907655fe061a25ff727f2a576e7ae9b13
alvexx45 <bernardoalves222@gmail.com>
2025-03-20 11:15:48 -0300

    lab02q01 done

--- lab/lab02/q1/main.c
+++ lab/lab02/q1/main.c
@@ -2,7 +2,28 @@
 #include <stdlib.h>
 #include <string.h>
 
+void combinar(char *str1, char *str2) {
+    char res[201] = "";
+    int i = 0, j = 0, k = 0;
+
+    while (i < strlen(str1) || j < strlen(str2)) {
+        if (i < strlen(str1)) res[k++] = str1[i++];
+        if (j < strlen(str2)) res[k++] = str2[j++];
+    }
+
+    printf("%s\n", res);
+}
+
 int main() {
+    char str1[101];
+    char str2[101];
+
+    scanf("%s %s", str1, str2);
+    combinar(str1, str2);
+    while (scanf("%s %s", str1, str2) != EOF) {
+        combinar(str1, str2);
+    }
+
 
     return 0;
 }
