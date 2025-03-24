6d38dca36509c9bb1114ed5fcaa3f530906654fb
alvexx45 <bernardoalves222@gmail.com>
2025-03-24 14:33:08 -0300

    placas de carro c

--- beecrowd/pendentes/OBI 2024 - Placas de Carro/main.c
+++ beecrowd/pendentes/OBI 2024 - Placas de Carro/main.c
@@ -1,10 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
+
+int padrao(char placa[11]) {
+    int res = 0;
+    int len = strlen(placa);
+
+    if (len == 8 && placa[3] == '-') {
+        for (int i = 0; i < 3; i++) {
+            if (!isalpha(placa[i])) {
+                i = 3;
+            }
+        }
+    
+        for (int i = 4; i < 8; i++) {
+            if (!isdigit(placa[i])) {
+                i = 8;
+            }
+        }
+
+        res = 1;
+    } 
+    else if (len == 7  && isalpha(placa[4])) {
+        for (int i = 0; i < 3; i++) {
+            if (!isalpha(placa[i])) {
+                i = 3;
+            }
+        }
+
+        for (int i = 3; i < 7; i++) {
+            if (!placa[4] && !isdigit(placa[i])) {
+                i = 7;
+            }
+        }
+
+        res = 2;
+    }
+
+    return res;
+}
 
 int main() {
-    
+    char placa[11];
 
+    scanf("%s", placa);
+    printf("%d\n", padrao(placa));
+    while (scanf("%s", placa) != EOF) {
+        printf("%d\n", padrao(placa));
+    }
 
     return 0;
 }
