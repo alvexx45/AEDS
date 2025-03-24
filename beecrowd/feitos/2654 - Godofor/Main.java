2e1059f99ee697a6bdd66e17a89fef41aa8b7d4d
alvexx45 <bernardoalves222@gmail.com>
2025-03-24 18:02:31 -0300

    godofor java remake

--- beecrowd/feitos/2654 - Godofor/Main.java
+++ beecrowd/feitos/2654 - Godofor/Main.java
@@ -1,46 +1,72 @@
 import java.util.Scanner;
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Collections;
 
 class Main {
+    public static void sort(Deuses d[], int n) {
+        for (int i = 0; i < n-1; i++) {
+            int maior = i;
+    
+            for (int j = i+1; j < n; j++) {
+                if (d[j].power > d[maior].power) {
+                    maior = j;
+                } else if (d[j].power == d[maior].power) {
+                    if (d[j].kills > d[maior].kills) {
+                        maior = j;
+                    }
+                } else if (d[j].kills == d[maior].kills) {
+                    if (d[j].mortes > d[maior].mortes) {
+                        maior = j;
+                    }
+                } else if (d[j].kills == d[maior].kills) {
+                    if (d[j].nome.compareTo(d[maior].nome) < 0) {
+                        maior = j;
+                    }
+                }
+            }
+
+            Deuses tmp = d[i];
+            d[i] = d[maior];
+            d[maior] = tmp;
+        }
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+    
         int n = sc.nextInt();
         sc.nextLine();
-        
-        List<Deuses> d = new ArrayList<>();
-
+    
+        // List<Deuses> d = new ArrayList<>();
+    
+        Deuses d[] = new Deuses[n];
         for (int i = 0; i < n; i++) {
             String nome = sc.next();
             int power = sc.nextInt();
             int kills = sc.nextInt();
             int mortes = sc.nextInt();
-            sc.nextLine();
-            
-            d.add(new Deuses(nome, power, kills, mortes));
+    
+            d[i] = new Deuses(nome, power, kills, mortes);
         }
-
-        Collections.sort(d, (d1, d2) -> {
-            if (d1.power != d2.power) {
-                return Integer.compare(d2.power, d1.power);
-            }
-            
-            if (d1.kills != d2.kills) {
-                return Integer.compare(d2.kills, d1.kills);
-            }
-            
-            if (d1.mortes != d2.mortes) {
-                return Integer.compare(d2.mortes, d1.mortes);
-            }
-            
-            return d1.nome.compareTo(d2.nome);
-        });
-
-        System.out.println(d.get(0).nome);
-
+        sort(d, n);
+    
+        System.out.println(d[0].nome);
+    
         sc.close();
+
+        // Collections.sort(d, (d1, d2) -> {
+        // if (d1.power != d2.power) {
+        // return Integer.compare(d2.power, d1.power);
+        // }
+        //
+        // if (d1.kills != d2.kills) {
+        // return Integer.compare(d2.kills, d1.kills);
+        // }
+        //
+        // if (d1.mortes != d2.mortes) {
+        // return Integer.compare(d2.mortes, d1.mortes);
+        // }
+        //
+        // return d1.nome.compareTo(d2.nome);
+        // });
     }
 }
 
