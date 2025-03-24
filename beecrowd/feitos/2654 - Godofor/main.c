1dd06efc8acd851b94eb9f31f0a96b100c8ad5fb
alvexx45 <bernardoalves222@gmail.com>
2025-03-24 17:26:11 -0300

    organizando

--- beecrowd/feitos/2654 - Godofor/main.c
+++ beecrowd/feitos/2654 - Godofor/main.c
@@ -7,14 +7,20 @@ typedef struct {
     int power, kills, mortes;
 } Deuses;
 
-int compare(const void *a, const void *b) {
-    Deuses *d1 = (Deuses *)a;
-    Deuses *d2 = (Deuses *)b;
+// int compare(const void *a, const void *b) {
+    // Deuses *d1 = (Deuses *)a;
+    // Deuses *d2 = (Deuses *)b;
+// 
+    // return d2->power - d1->power;
+    // return d2->kills - d1->kills;
+    // return d2->mortes - d1->mortes;
+    // return strcmp(d1->nome, d2->nome);
+// }
 
-    return d2->power - d1->power;
-    return d2->kills - d1->kills;
-    return d2->mortes - d1->mortes;
-    return strcmp(d1->nome, d2->nome);
+void sort (Deuses d[], int n) {
+    for (int i = 0; i < n-1; i++) {
+        int menor = i;
+    }
 }
 
 int main() {
@@ -26,9 +32,9 @@ int main() {
     for (int i = 0; i < n; i++) {
         scanf("%s %d %d %d", d[i].nome, &d[i].power, &d[i].kills, &d[i].mortes);
     }
+    sort(d, n);
 
-    
-    qsort(d, n, sizeof(Deuses), compare);
+    // qsort(d, n, sizeof(Deuses), compare);
     printf("%s", d[0].nome);
 
     return 0;
