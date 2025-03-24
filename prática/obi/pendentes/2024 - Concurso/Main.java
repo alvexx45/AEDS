bc2ec3a06aea4689d4e8d214fe9488f4017d5aca
alvexx45 <bernardoalves222@gmail.com>
2025-03-24 19:39:42 -0300

    concurso java

--- prática/obi/pendentes/2024 - Concurso/Main.java
+++ prática/obi/pendentes/2024 - Concurso/Main.java
@@ -1,8 +1,37 @@
 import java.util.Scanner;
 
 class Main {
+    public static int corte(int array[], int n, int k) {
+        for (int i = 0; i < k; i++) {
+            int maior = i;
+            for (int j = i+1; j < n; j++) {
+                if (array[j] > array[maior]) {
+                    maior = j;
+                }
+            }
+            int tmp = array[i];
+            array[i] = array[maior];
+            array[maior] = tmp;
+        }
+    
+        return array[k-1];
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        int n, k;
+        while(sc.hasNextInt()) {
+            n = sc.nextInt();
+            k = sc.nextInt();
+            
+            int[] array = new int[n];
+            for (int i = 0; i < n; i++) {
+                array[i] = sc.nextInt();
+            }
+            
+            System.out.println(corte(array, n, k));
+        }
 
         sc.close();
     }
