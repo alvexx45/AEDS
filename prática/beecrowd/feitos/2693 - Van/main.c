91e4697c5ba7956d748841fcde5b8a1ef45f57f1
alvexx45 <bernardoalves222@gmail.com>
2025-03-25 10:53:48 -0300

    van c update

--- prática/beecrowd/feitos/2693 - Van/main.c
+++ prática/beecrowd/feitos/2693 - Van/main.c
@@ -8,7 +8,7 @@ typedef struct {
     int dist;
 } Van;
 
-int compare(const void *a, const void *b) {
+/* int compare(const void *a, const void *b) {
     Van *v1 = (Van *)a;
     Van *v2 = (Van *)b;
 
@@ -16,6 +16,22 @@ int compare(const void *a, const void *b) {
         return v1->dist - v2->dist;
         
     return strcmp(v1->nome, v2->nome);
+} */
+
+void sort(Van v[], int n) {
+    for (int i = 0; i < n-1; i++) {
+        int menor = i;
+        for (int j = i+1; j < n; j++) {
+            if(v[j].dist < v[menor].dist ||
+                (v[j].dist == v[menor].dist && v[j].dir < v[menor].dir) ||
+                (v[j].dir == v[menor].dir && strcmp(v[j].nome, v[menor].nome) < 0)){
+                    menor = j;
+                }
+        }
+        Van tmp = v[i];
+        v[i] = v[menor];
+        v[menor] = tmp;
+    }
 }
 
 int main() {
@@ -27,7 +43,8 @@ int main() {
     for (int i = 0; i < n; i++) {
         scanf("%s %c %d", v[i].nome, &v[i].dir, &v[i].dist);
     }
-    qsort(v, n, sizeof(Van), compare);
+    sort(v, n);
+    // qsort(v, n, sizeof(Van), compare);
 
     for (int i = 0; i < n; i++) {
         printf("%s\n", v[i].nome);
