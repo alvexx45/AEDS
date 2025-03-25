bdf7c4cc0e161eca24ff185a9b54bae8c8c879ef
alvexx45 <bernardoalves222@gmail.com>
2025-03-25 09:25:37 -0300

    ogro java

--- prática/obi/pendentes/2024 - Ogro/Main.java
+++ prática/obi/pendentes/2024 - Ogro/Main.java
@@ -1,8 +1,23 @@
 import java.util.Scanner;
 
 class Main {
+    public static int operacao(int e, int d) {
+        int res = 0;
+        
+        if (e > d) {
+            res = e + d;
+        } else {
+            res = 2 * (d - e);
+        }
+
+        return res;
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        int e = sc.nextInt();
+        int d = sc.nextInt();
+        System.out.println(operacao(e, d));
 
         sc.close();
     }
