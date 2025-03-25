7ac43df2af2fe909788978c6f0bbbfa0d3efda43
alvexx45 <bernardoalves222@gmail.com>
2025-03-25 11:42:39 -0300

    experiencias java

--- prática/beecrowd/pendentes/1094 - Experiencias/Main.java
+++ prática/beecrowd/pendentes/1094 - Experiencias/Main.java
@@ -1,8 +1,10 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
 
         int n = sc.nextInt();
         Cobaias[] c = new Cobaias[n];
@@ -15,11 +17,26 @@ class Main {
             c[i] = new Cobaias(qtd, tipo);
 
             if (c[i].tipo == 'C') {
-                
+                coelhos += c[i].qtd;
+            } else if (c[i].tipo == 'R') {
+                ratos += c[i].qtd;
+            } else {
+                sapos += c[i].qtd;
             }
         }
-
         int soma = coelhos + ratos + sapos;
+
+        double pCoelhos = (coelhos * 100.0) / soma;
+        double pRatos = (ratos * 100.0) / soma;
+        double pSapos = (sapos * 100.0) / soma;
+
+        System.out.printf("Total: %d cobaias\n", soma);
+        System.out.printf("Total de coelhos: %d\n", coelhos);
+        System.out.printf("Total de ratos: %d\n", ratos);
+        System.out.printf("Total de sapos: %d\n", sapos);
+        System.out.printf("Percentual de coelhos: %.2f %%\n", pCoelhos);
+        System.out.printf("Percentual de ratos: %.2f %%\n", pRatos);
+        System.out.printf("Percentual de sapos: %.2f %%\n", pSapos);
 
         sc.close();
     }
