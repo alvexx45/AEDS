694c4b464ade01f1195ba46126fc524c998f7446
alvexx45 <bernardoalves222@gmail.com>
2025-04-16 16:48:57 -0300

    celula

---
+++ codigos/tads/flex/Celula.java
@@ -0,0 +1,11 @@
+class Celula {
+    int elemento;
+    Celula prox;
+
+    public Celula() { this(0); }
+    
+    public Celula(int x) {
+        this.elemento = x;
+        this.prox = null;
+    }
+}
