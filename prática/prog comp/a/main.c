b02c1a17f26317cef960a5ecf16068716b6c25bd
alvexx45 <bernardoalves222@gmail.com>
2025-04-22 13:43:52 -0300

    create prog comp

---
+++ prática/prog comp/a/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+    int c;
+    scanf("%d", &c);
+
+    for (int i = 0; i < c; i++) {
+        
+    }
+
+    return 0;
+}
