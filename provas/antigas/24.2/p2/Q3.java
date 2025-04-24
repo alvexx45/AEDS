23177e278ab41c0d1e0318ec12035428123c9353
alvexx45 <bernardoalves222@gmail.com>
2025-04-24 19:31:56 -0300

    Q3 done

--- provas/antigas/24.2/p2/Q3.java
+++ provas/antigas/24.2/p2/Q3.java
@@ -3,7 +3,19 @@ class Q3 {
         ListaDupla l1 = new ListaDupla();
         ListaDupla l2 = new ListaDupla();
 
+        l1.inserirFim(3);
+        l1.inserirFim(7);
+        l1.inserirFim(5);
 
+        l2.inserirFim(1);
+        l2.inserirFim(9);
+
+        CelulaDupla c3 = new CelulaDupla();
+        CelulaDupla res = c3.intercalarReverso(l1.primeiro, l2.primeiro);
+
+        ListaDupla l3 = new ListaDupla();
+        l3.primeiro.prox = res.prox;
+        l3.mostrar();
     }
 }
 
@@ -19,15 +31,52 @@ class CelulaDupla {
     }
 
     CelulaDupla intercalarReverso(CelulaDupla c1, CelulaDupla c2) {
-        Celula c3 = new CelulaDupla();
+        ListaDupla l3 = new ListaDupla();
 
-        
+        CelulaDupla u1 = c1;
+        while (u1.prox != null) {
+            u1 = u1.prox;
+        }
 
-        return c3;
+        CelulaDupla u2 = c2;
+        while (u2.prox != null) {
+            u2 = u2.prox;
+        }
+
+        while (u1 != c1 || u2 != c2) {
+            if (u1 != c1) {
+                l3.inserirFim(u1.elemento);
+                u1 = u1.ant;
+            }
+            if (u2 != c2) {
+                l3.inserirFim(u2.elemento);
+                u2 = u2.ant;
+            }
+        }
+
+        return l3.primeiro;
     }
 }
 
 class ListaDupla {
     int elemento;
     CelulaDupla primeiro, ultimo;
+
+    ListaDupla() {
+        primeiro = new CelulaDupla();
+        ultimo = primeiro;
+    }
+
+    void inserirFim(int x) {
+        ultimo.prox = new CelulaDupla(x);
+        ultimo.prox.ant = ultimo;
+        ultimo = ultimo.prox;
+    }
+
+    void mostrar() {
+        for (CelulaDupla i = primeiro.prox; i != null; i = i.prox) {
+            System.out.print(i.elemento + " ");
+        }
+        System.out.println();
+    }
 } 
