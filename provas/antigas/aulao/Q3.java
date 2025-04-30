8899442663f474316b3dbf4367be8d9b9217eabf
alvexx45 <bernardoalves222@gmail.com>
2025-04-30 15:43:40 -0300

    q3 aulao

--- provas/antigas/aulao/Q3.java
+++ provas/antigas/aulao/Q3.java
@@ -1,3 +1,42 @@
-class Q3 {
-    
+class CelulaMat {
+    CelulaMat inf, sup, esq, dir;
+    CelulaLista inicio, fim;
+}
+
+class CelulaLista {
+    int elemento;
+    CelulaLista prox;
+}
+
+class Matriz {
+    CelulaMat inicio;
+
+    void percorrer() {
+        CelulaMat lin = inicio, col = inicio;
+ 
+        while (lin != null) {
+            while (col != null) {
+                remover(col);
+                col = col.dir;
+            }
+            lin = lin.inf;
+            col = lin;
+        }
+    }
+
+    void remover(CelulaMat i) {
+        CelulaLista tmp = new CelulaLista();
+
+        tmp.prox = i.inicio;
+        CelulaLista j = tmp;
+
+        while (j.prox != null) {
+            if (j.prox.elemento % 2 != 0) {
+                j.prox = j.prox.prox;
+            } else {
+                j = j.prox;
+            }
+        }
+        i.inicio = tmp.prox;
+    }
 }
