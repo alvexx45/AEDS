5bb3125ac44a5d77b0061e6a95eb2e9e64100d85
alvexx45 <bernardoalves222@gmail.com>
2025-04-30 14:33:26 -0300

    q1 done

--- provas/antigas/aulao/Q1.java
+++ provas/antigas/aulao/Q1.java
@@ -2,7 +2,29 @@ class Lista {
     CelulaLista inicio, fim;
 
     CelulaLista maiorPilha() {
-        
+        CelulaLista resp = inicio;
+        int maior = 0;
+
+        if (inicio == fim) return null;
+
+        for (CelulaLista i = inicio; i != null; i = i.prox) {
+            int j = tamanhoPilha(i);
+
+            if (j > maior) {
+                resp = i;
+                maior = j;
+            }
+        }
+
+        return resp;
+    }
+
+    int tamanhoPilha(CelulaLista i) {
+        int cont = 0;
+
+        for (CelulaPilha j = i.topo; j != null; j = j.prox) cont++;
+
+        return cont;
     }
 }
 
