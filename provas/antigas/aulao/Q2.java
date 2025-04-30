3eb15698336ae68f1a0a63cd2b1762520da3a0a8
alvexx45 <bernardoalves222@gmail.com>
2025-04-30 15:07:37 -0300

    q2 aulao

--- provas/antigas/aulao/Q2.java
+++ provas/antigas/aulao/Q2.java
@@ -1,3 +1,29 @@
-public class Q2 {
-    
+class Celula {
+    int elemento;
+    Celula prox;
+
+    Celula() { this(0); }
+
+    Celula(int x) {
+        this.elemento = x;
+        this.prox = null;
+    }
+}
+
+class Lista {
+    Celula inicio, fim;
+
+    void inverter() {
+        Celula a = null, b = null;
+        Celula i = inicio;
+        fim = inicio;
+
+        while (i != null) {
+            a = i.prox;
+            i.prox = b;
+            b = i;
+            i = a;
+        }
+        inicio = b;
+    }
 }
