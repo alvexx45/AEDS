3c0d3d842abdb3f9dace1af5ff6ef9895e6915d4
alvexx45 <bernardoalves222@gmail.com>
2025-04-30 14:20:12 -0300

    create q1

---
+++ provas/antigas/aulao/Q1.java
@@ -0,0 +1,18 @@
+class Lista {
+    CelulaLista inicio, fim;
+
+    CelulaLista maiorPilha() {
+        
+    }
+}
+
+class CelulaLista {
+    CelulaPilha topo;
+    CelulaLista prox;
+
+}
+
+class CelulaPilha {
+    int elemento;
+    CelulaPilha prox;
+}
