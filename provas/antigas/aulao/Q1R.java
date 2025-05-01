b1706b47a20f0e852eb45d54078a27d34c9358ab
alvexx45 <bernardoalves222@gmail.com>
2025-05-01 09:50:21 -0300

    q1 refeita

---
+++ provas/antigas/aulao/Q1R.java
@@ -0,0 +1,39 @@
+class Lista {
+    CelulaLista inicio, fim;
+    
+    CelulaLista maiorPilha() {
+        CelulaLista resp = inicio;
+        int maior = 0;
+
+        if (inicio == fim) return null;
+
+        for (CelulaLista i = inicio; i != null; i = i.prox) {
+            int j = tamanho(i);
+
+            if (j > maior) {
+                resp = i;
+                maior = j;
+            }
+        }
+
+        return resp;
+    }
+
+    int tamanho(CelulaLista cel) {
+        int size = 0;
+
+        for (CelulaPilha i = cel.topo; i != null; i = i.prox) size++;
+
+        return size;
+    }
+}
+
+class CelulaLista {
+    CelulaPilha topo;
+    CelulaLista prox;
+}
+
+class CelulaPilha {
+    int elemento;
+    CelulaPilha prox;
+}
