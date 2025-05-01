0eded01b6c4d6569cee2b1593d51de4649596cb4
alvexx45 <bernardoalves222@gmail.com>
2025-05-01 09:57:49 -0300

    create q4

--- provas/antigas/aulao/Q4.java
+++ provas/antigas/aulao/Q4.java
@@ -1,3 +1,23 @@
-public class Q4 {
-    
+class CelulaDupla {
+    int elemento;
+    CelulaDupla ant, prox;
+
+    CelulaDupla intercalarReverso(CelulaDupla c1, CelulaDupla c2) {
+        CelulaDupla c3 = null;
+        
+        CelulaDupla u1, u2;
+
+        for (u1 = c1; c1 != null; u1 = u1.prox);
+        for (u2 = c2; c2 != null; u2 = u2.prox);
+
+        while (u1 != c1 || u2 != c2) {
+
+        }
+
+        return c3;
+    }
+
+    int removerFim() {
+        int resp;
+    }
 }
