036d3e61895f2c114c7fda055a22c0c8fe4c194b
alvexx45 <bernardoalves222@gmail.com>
2025-05-01 11:35:38 -0300

    q4 done

--- provas/antigas/aulao/Q4.java
+++ provas/antigas/aulao/Q4.java
@@ -2,22 +2,48 @@ class CelulaDupla {
     int elemento;
     CelulaDupla ant, prox;
 
+    public CelulaDupla() { this(0); }
+
+    public CelulaDupla(int x) {
+        this.elemento = x;
+        this.ant = this.prox = null;
+    }
+
     CelulaDupla intercalarReverso(CelulaDupla c1, CelulaDupla c2) {
-        CelulaDupla c3 = null;
+        ListaDupla l3 = new ListaDupla();
         
         CelulaDupla u1, u2;
 
-        for (u1 = c1; c1 != null; u1 = u1.prox);
-        for (u2 = c2; c2 != null; u2 = u2.prox);
+        for (u1 = c1; u1 != null; u1 = u1.prox);
+        for (u2 = c2; u2 != null; u2 = u2.prox);
 
         while (u1 != c1 || u2 != c2) {
-
+            if (u1 != c1) {
+                l3.inserirFim(u1.elemento);
+                u1 = u1.ant;
+            }
+            if (u2 != c2) {
+                l3.inserirFim(u2.elemento);
+                u2 = u2.ant;
+            }
         }
 
-        return c3;
+        return l3.primeiro;
     }
 
-    int removerFim() {
-        int resp;
+}
+
+class ListaDupla {
+    CelulaDupla primeiro, ultimo;
+
+    ListaDupla() {
+        primeiro = new CelulaDupla();
+        ultimo = primeiro;
+    }
+
+    void inserirFim(int x) {
+        ultimo.prox = new CelulaDupla(x);
+        ultimo.prox.ant = ultimo;
+        ultimo = ultimo.prox;
     }
 }
