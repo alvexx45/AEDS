da8ab1a2b30df6be8c89178590bfe1856d99e335
alvexx45 <bernardoalves222@gmail.com>
2025-05-03 13:30:59 -0300

    p1q1

--- provas/antigas/outras/p1/Q1.java
+++ provas/antigas/outras/p1/Q1.java
@@ -10,4 +10,29 @@ class CelulaPilha {
 
 class Lista {
     CelulaLista inicio, fim;
+
+    CelulaLista percorrer() {
+        CelulaLista resp = inicio;
+        int maior = 0;
+        
+        if (inicio == fim) return null;
+
+        for (CelulaLista i = inicio; i != null; i = i.prox) {
+            int j = tamanho(i);
+            if (j > maior) {
+                resp = i;
+                maior = j;
+            }
+        }
+
+        return resp;
+    }
+
+    int tamanho(CelulaLista cel) {
+        int size = 0;
+        
+        for (CelulaPilha i = cel.topo; i != null; i = i.prox) size++;
+
+        return size;
+    }
 }
