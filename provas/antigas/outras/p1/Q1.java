f474c3dcfeb5a026b02ba0f137246fca3f11af26
alvexx45 <bernardoalves222@gmail.com>
2025-05-03 13:18:19 -0300

    create q1

---
+++ provas/antigas/outras/p1/Q1.java
@@ -0,0 +1,13 @@
+class CelulaLista {
+    CelulaPilha topo;
+    CelulaLista prox;
+}
+
+class CelulaPilha {
+    int elemento;
+    CelulaPilha prox;
+}
+
+class Lista {
+    CelulaLista inicio, fim;
+}
