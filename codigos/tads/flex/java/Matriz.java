11c561ce81b46d17e326b9cb68df6805e8c13882
alvexx45 <bernardoalves222@gmail.com>
2025-05-04 16:26:16 -0300

    metodos classe matriz

--- codigos/tads/flex/java/Matriz.java
+++ codigos/tads/flex/java/Matriz.java
@@ -5,6 +5,71 @@ class Matriz {
 }
 
 class CriarMatriz {
-    int elemento;
-    CelulaMat inf, sup, esq, dir;
+    CelulaMat inicio;
+    int linhas, colunas;
+
+    void removerColuna() {
+        if (colunas == 0) {
+            return;
+        } else if (colunas == 1) {
+            inicio = null;
+            linhas--;
+        } else {
+            CelulaMat i;
+            for (i = inicio; i.dir != null; i = i.dir);
+
+            while (i != null) {
+                i.esq = i.esq.dir = null;
+                i = i.inf;
+            }
+            colunas--;
+        }
+    }
+
+    void removerLinha() {
+        if (linhas == 0) {
+            return;
+        } else if (linhas == 1) {
+            inicio = null;
+            linhas--;
+        } else {
+            CelulaMat i;
+            for (i = inicio; i.inf != null; i = i.inf);
+
+            while (i != null) {
+                i.sup = i.sup.inf = null;
+                i = i.dir;
+            }
+            linhas--;
+        }
+    }
+
+    void getDiagPrincipal() {
+        if (linhas != colunas || inicio == null) return;
+
+        CelulaMat i = inicio;
+        
+        while (i != null) {
+            System.out.print(i.elemento + " ");
+            i = i.dir;
+            if (i != null) {
+                i = i.inf;
+            }
+        }
+    }
+
+    void getDiagSecundaria() {
+        if (linhas != colunas || inicio == null) return;
+
+        CelulaMat i;
+        for (i = inicio ; i.dir != null; i = i.dir);
+
+        while (i != null) {
+            System.out.print(i.elemento + " ");
+            i = i.esq;
+            if (i != null) {
+                i = i.inf;
+            }
+        }
+    }
 }
