b21a82a43b4eba5c2964de63932e5e0ed2b0ba35
alvexx45 <bernardoalves222@gmail.com>
2025-05-04 16:17:23 -0300

    p4q3

--- provas/antigas/outras/p4/P4Q3.java
+++ provas/antigas/outras/p4/P4Q3.java
@@ -1,3 +1,45 @@
-CelulaDupla {
+class Matriz {
+    CelulaMatriz inicio;
+    int linhas, colunas;
     
+    CelulaDupla diagUnificada() {
+        if (linhas != colunas || inicio == null) return null; 
+        
+        CelulaDupla cabecaLD = new CelulaDupla();
+        CelulaDupla ultimoLD = cabecaLD;
+    
+        CelulaMatriz i = inicio;
+        while (i != null) {
+            for (Celula j = i.inicio.prox; j != null; j = j.prox) {
+                CelulaDupla tmp = new CelulaDupla();
+
+                tmp.elemento = j.elemento;
+                tmp.ant = ultimoLD;
+                ultimoLD.prox = tmp;
+                ultimoLD = tmp;
+            }
+
+            i = i.dir;
+            if (i != null) {
+                i = i.inf;
+            }
+        }
+    
+        return cabecaLD;
+    }
+}
+
+class CelulaMatriz {
+    CelulaMatriz esq, dir, inf, sup;
+    Celula inicio, fim;
+}
+
+class Celula {
+    int elemento;
+    Celula prox;
+}
+
+class CelulaDupla {
+    int elemento;
+    CelulaDupla prox, ant;
 }
