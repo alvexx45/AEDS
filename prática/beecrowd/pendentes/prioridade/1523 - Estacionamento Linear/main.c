549c60a2b7df92d0ff9dcc1188ecd7e7ae605680
alvexx45 <bernardoalves222@gmail.com>
2025-05-13 21:23:30 -0300

    1523

--- prática/beecrowd/pendentes/prioridade/1523 - Estacionamento Linear/main.c
+++ prática/beecrowd/pendentes/prioridade/1523 - Estacionamento Linear/main.c
@@ -4,34 +4,66 @@
 #include <stdbool.h>
 
 typedef struct Celula {
-    int cheg, saida;
+    int saida;
     struct Celula *prox;
 } Celula;
 
+Celula *novaCelula(int x) {
+    Celula *nova = (Celula *) malloc (sizeof(Celula));
+    nova->saida = x;
+    nova->prox = NULL;
+    
+    return nova;
+}
+
 Celula *topo = NULL;
+int tamanho = 0;
 
-bool lotado(int n, int k) {
-    bool resp = false;
+void inserir(int x) {
+    Celula *tmp = novaCelula(x);
+    tmp->prox = topo;
+    topo = tmp;
+    tamanho++;
+}
 
+int remover() {
+    if (topo == NULL) printf("pilha vazia");
 
+    int resp = topo->saida;
+    topo = topo->prox;
+    tamanho--;
 
     return resp;
 }
-
-
 
 int main() {
     int n, k;
     scanf("%d %d", &n, &k);
 
     while(n != 0 && k != 0) {
+        bool livre = true;
+
         for (int i = 0; i < n; i++) {
-            scanf("%d %d", &m[i].cheg, &m[i].saida);
+            int cheg, saida;
+            scanf("%d %d", &cheg, &saida);
+
+            while (topo != NULL && cheg >= topo->saida) remover();
+
+            if (topo != NULL && saida > topo->saida) livre = false;
+            if (tamanho >= k) livre = false;
+            if (livre) inserir(saida);
         }
-        if (lotado(m, n, k)) {
+
+        if (livre) {
             printf("Sim\n");
         } else {
             printf("Nao\n");
+        }
+
+        while (topo != NULL) {
+            Celula *tmp = topo;
+            topo = topo->prox;
+            free(tmp);
         }
 
         scanf("%d %d", &n, &k);
