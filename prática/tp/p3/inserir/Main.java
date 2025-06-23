ff2db00b3cb31f93c1e77a11e32af1a4e369a4b4
alvexx45 <bernardoalves222@gmail.com>
2025-06-23 16:04:32 -0300

    bst p3 done

--- prática/tp/p3/inserir/Main.java
+++ prática/tp/p3/inserir/Main.java
@@ -3,18 +3,73 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        
+        Arvore arvore = new Arvore();
+        
+        String line = sc.nextLine();
+        while (!line.equals("FIM")) {
+            Carro carro = new Carro();
+            carro.ler(line);
+            arvore.inserir(carro);
+            
+            line = sc.nextLine();
+        }
+
+        arvore.caminhar();
 
         sc.close();
     }
 }
 
-class Arvore {
+class No {
+    Carro carro;
+    No esq, dir;
 
+    No(Carro carro) {
+        this.carro = carro;
+        this.esq = this.dir = null;
+    }
+}
+
+class Arvore {
+    No raiz;
+
+    Arvore() {
+        this.raiz = null;
+    }
+
+    void inserir(Carro x) {
+        raiz = inserir(x, raiz);
+    }
+
+    No inserir(Carro x, No i) {
+        if (i == null) {
+            i = new No(x);
+        } else if (x.placa.compareTo(i.carro.placa) < 0) {
+            i.esq = inserir(x, i.esq);
+        } else if (x.placa.compareTo(i.carro.placa) > 0) {
+            i.dir = inserir(x, i.dir);
+        }
+
+        return i;
+    }
+
+    void caminhar() {
+        caminhar(raiz);
+    }
+
+    void caminhar(No i) {
+        if (i != null) {
+            caminhar(i.esq);
+            i.carro.imprimir();
+            caminhar(i.dir);
+        }
+    }
 }
 
 class Carro {
     String placa, modelo, tipo, chassi;
-
+    
     void ler(String linha) {
         String[] partes = linha.split(",");
         placa = partes[0];
